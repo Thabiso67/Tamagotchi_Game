59ff0924147f559f8e3a3f158f0bb7dab06bf787
Thabiso67 <st10444690@vcconnect.edu.za>
2024-05-04 20:19:46 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tamagotchi/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.tamagotchi
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.ImageDecoder
+import android.graphics.drawable.AnimatedImageDrawable
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.annotation.RequiresApi
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val ImageView: ImageView = findViewById(R.id.welcome)
+        val Welcome: ImageView = findViewById(R.id.welcome_dog)
+        val Button: Button = findViewById(R.id.button_start)
+
+        Button.setOnClickListener{
+
+            val intent = Intent(this,MainActivity2::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/tamagotchi/MainActivity2.kt
@@ -0,0 +1,33 @@
+package com.example.tamagotchi
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import org.w3c.dom.Text
+
+class MainActivity2 : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        val ImageView: ImageView = findViewById(R.id.welcome_dog)
+        val
+        val Feed: Button = findViewById(R.id.feed)
+        val Clean: Button = findViewById(R.id.clean)
+        val Play: Button = findViewById(R.id.play)
+        val Hunger: TextView = findViewById(R.id.textView1)
+        val Mood: TextView = findViewById(R.id.textView2)
+        val Dirt: TextView = findViewById(R.id.textView3)
+        val bundle: Bundle? =intent.extras
+
+        Feed.setOnClickListener{
+
+
+        }
+
+    }
+}
