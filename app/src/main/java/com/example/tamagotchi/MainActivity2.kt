da6fab8a0a8ddc5d2e77e2110bd63dfacd71012d
Thabiso67 <st10444690@vcconnect.edu.za>
2024-05-05 10:51:11 +0200

    Activated Action buttons

--- app/src/main/java/com/example/tamagotchi/MainActivity2.kt
+++ app/src/main/java/com/example/tamagotchi/MainActivity2.kt
@@ -15,19 +15,28 @@ class MainActivity2 : AppCompatActivity() {
         setContentView(R.layout.activity_main2)
 
         val ImageView: ImageView = findViewById(R.id.welcome_dog)
-        val
         val Feed: Button = findViewById(R.id.feed)
         val Clean: Button = findViewById(R.id.clean)
         val Play: Button = findViewById(R.id.play)
         val Hunger: TextView = findViewById(R.id.textView1)
         val Mood: TextView = findViewById(R.id.textView2)
         val Dirt: TextView = findViewById(R.id.textView3)
-        val bundle: Bundle? =intent.extras
+        val bundle: Bundle? = intent.extras
 
-        Feed.setOnClickListener{
+        Feed.setOnClickListener {
 
+            ImageView.setImageResource(R.drawable.feed)
 
+            Play.setOnClickListener {
+
+                ImageView.setImageResource(R.drawable.play)
+
+                Clean.setOnClickListener {
+
+                    ImageView.setImageResource(R.drawable.clean)
+
+                }
+            }
         }
-
     }
 }
